19e2b20e6c45044c2018078ccfe3ffef8f3fa8d9
Ben Tossell <12862117+bentosseII@users.noreply.github.com>
2026-03-03 21:45:44 -0500

    Add src/core/scoring/index.ts

---
+++ src/core/scoring/index.ts
@@ -0,0 +1,4 @@
+export { aggregateEpisodeScore, maybeComputeMemoryScore } from './aggregator'
+export { LLMJudge, type LLMJudgeConfig } from './llmJudge'
+export { computeMemoryScore } from './memory'
+export { failureModeDescriptions } from './taxonomy'
